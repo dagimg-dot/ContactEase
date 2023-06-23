25671dc904eb53079b159e786d31f02a9df63468
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-24 00:27:59 +0300

    trial one

--- server/express/say-hi.js
+++ server/express/say-hi.js
@@ -1,6 +1,8 @@
-export default (req, res) => {
+const say_hi = (req, res) => {
   const { name } = req.body.input;
   return res.status(200).json({
     message: "hello " + name,
   });
 };
+
+module.exports = say_hi;
