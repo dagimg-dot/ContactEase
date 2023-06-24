3802711c4ad20d36baebeacbd9ba4c6d3e620b68
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-24 14:05:18 +0300

    create the company logo component

---
+++ components/CompanyLogo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper flex items-center gap-3" @click="redirectToHomePage">
+        <CompanyLogoIcon />
+        <div class="text-red-700 text-[30px] font-bold">Contact Ease</div>
+    </div>
+</template>
+
+<script setup>
+    const router = useRouter();
+    const redirectToHomePage = () => {
+        router.push("/");
+    };
+
+</script>
+
+<style scoped>
+.wrapper {
+    cursor: pointer;
+}
+</style>
