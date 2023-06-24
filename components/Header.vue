5a759aa44cc6249b604cf06c8228b24fdbea50b6
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-24 14:05:30 +0300

    create the header component

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-between items-center">
+      <CompanyLogo />
+      <div class="flex gap-4">
+        <SignIn />
+        <SignUp />
+      </div>
+    </div>
+  </template>  
+
+<script setup>
+
+</script>
+
+<style scoped></style>
