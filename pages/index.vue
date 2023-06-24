6bfed5f011de86a83ff61db954597443a0889f23
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-24 14:05:54 +0300

    finish the landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,14 @@
 <template>
-  <div class="flex flex-wrap">
-    <div v-for="user in users">
-      <p>{{ user.username }}</p>
+  <div class="landing-page flex flex-col h-screen">
+    <Header class="mx-11 my-5" />
+    <div class="flex flex-col justify-center flex-grow gap-10">
+      <LandingText />
+      <GetStarted />
     </div>
   </div>
 </template>
 
+
 <script setup>
-
-import user from '../apollo/queries/fetchUser.gql'
-
-const { data } = await useAsyncQuery(user)
-
-const users = data.value.user
-
-try {
-  console.log(data.value.user[0].username)
-} catch (error) {
-  console.log(error)
-}
 </script>
+<style scoped></style>
