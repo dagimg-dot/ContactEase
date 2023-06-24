bd965b037df3cb07489c5bcce352b4441905ca5d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-24 14:05:40 +0300

    create the landing page text component

---
+++ components/LandingText.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mx-auto max-w-[70%] flex flex-col justify-center items-center relative">
+        <div class="text-center text-black-400 text-[5em] font-extrabold">
+            We Will Help You Build Your Connections
+        </div>
+        <div class="text-center text-black-400 text-[22px] font-normal">
+            Unleash the flow, Let connections Grow
+        </div>
+        <div class="absolute -top-20 -right-8">
+            <svg width="43" height="47" viewBox="0 0 43 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.318279 0.195169L42.4393 20.5131L3.7829 46.832L0.318279 0.195169Z" fill="#3FC3DA" />
+            </svg>
+
+        </div>
+        <div class="absolute -bottom-20 -left-8">
+            <svg width="43" height="47" viewBox="0 0 43 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.318279 0.195169L42.4393 20.5131L3.7829 46.832L0.318279 0.195169Z" fill="#3FC3DA" />
+            </svg>
+
+        </div>
+        <div class="absolute -bottom-20 -right-8">
+            <svg width="92" height="92" viewBox="0 0 92 92" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="32.3009" width="67.095" height="67.095" transform="rotate(28.7782 32.3009 0)" fill="#FDB917" />
+            </svg>
+        </div>
+        <div class="absolute -top-20 -left-8">
+            <svg width="92" height="92" viewBox="0 0 92 92" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="32.3009" width="67.095" height="67.095" transform="rotate(28.7782 32.3009 0)" fill="#FDB917" />
+            </svg>
+        </div>
+    </div>
+</template>
+  
+  
+<script setup>
+
+</script>
+  
+<style scoped></style>
