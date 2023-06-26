5662f9b69165c8b5749bf8379335f0ba71a62ff2
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-26 19:00:35 +0300

    create main modal file

---
+++ modals/Modal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal'
+
+defineProps<{
+    title?: string
+}>()
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', modelValue: boolean): void
+}>()
+</script>
+
+<template>
+    <VueFinalModal class="flex items-start justify-center"
+        content-class="flex flex-col mt-12 p-4 text-red-800 rounded-lg border border-blue-300 shadow-lg bg-white "
+        @update:model-value="val => emit('update:modelValue', val)">
+        <div class="flex items-center justify-between">
+            <h3 v-if="title" class="text-2xl ">
+                {{ title }}
+            </h3>
+            <ClientOnly>
+                <button class="ml-auto" @click="emit('update:modelValue', false)">
+                    <div class="bg-white rounded-full">
+                        <CloseIcon class="cursor-pointer " />
+                    </div>
+                </button>
+            </ClientOnly>
+        </div>
+        <slot />
+    </VueFinalModal>
+</template>
+
+<style scoped>
+</style>
