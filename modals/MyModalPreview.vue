46bb7e093f85d83fbd39f9905356a38cbb8a3a50
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-26 19:01:10 +0300

    create trial of modal

---
+++ modals/MyModalPreview.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useModal } from 'vue-final-modal'
+import Modal from './Modal.vue'
+
+const { open } = useModal({
+    component: Modal,
+    attrs: {
+        title: 'Hello World!',
+    },
+    slots: {
+        default: '<p>The content of the modal</p>',
+    },
+})
+</script>
+
+<template>
+    <button @click="() => open()">
+        <!-- Open Modal -->
+    </button>
+</template>
