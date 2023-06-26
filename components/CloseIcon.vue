beea7b3e02a3068cc6676884495089b0a5d21958
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-26 19:00:13 +0300

    create close ison component for reuse

---
+++ components/CloseIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg class="h-5 w-5 text-red-400 hover:text-red-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20" fill="red">
+        <path fill-rule="evenodd"
+            d="M10.707 10l4.147-4.146a.5.5 0 10-.708-.708L10 9.293 5.854 5.146a.5.5 0 00-.708.708L9.293 10l-4.147 4.146a.5.5 0 10.708.708L10 10.707l4.146 4.147a.5.5 0 00.708-.708L10.707 10z"
+            clip-rule="evenodd" />
+    </svg>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
