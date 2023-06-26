3760a812f066d651c4bd7c3137e3d25f350aa2f8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-26 03:45:13 +0300

    rename button components

---
+++ components/SignInButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button
+        class="px-[26px] py-[10px] bg-gradient-to-r from-red-700 to-red-600 rounded-lg shadow shadow-inner justify-start items-start gap-[10px] inline-flex text-white text-[20px] font-semibold shadow-md hover:bg-red-800 hover:from-red-800 hover:to-red-700"
+        @click="redirect">
+        Log In
+    </button>
+</template>
+
+<script setup>
+const router = useRouter();
+
+const redirect = () => {
+    // To be implemented later
+    // if (localStorage.getItem("token")) {
+    //     console.log("token exists");
+    //     router.push("/about");
+    // } else {
+    //     router.push("/login");
+    // }
+
+    // for now
+    router.push("/login");
+};
+
+</script>
+
+<style scoped></style>
+
