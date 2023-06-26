6c8180de69e1e2b524765abd67dfae7443949623
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-06-26 03:46:14 +0300

    add method to change route when get started is clicked

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,17 @@
     <Header class="mx-11 my-5" />
     <div class="flex flex-col justify-center flex-grow gap-10">
       <LandingText />
-      <GetStarted />
+      <GetStartedButton @click="redirectToSignUp"/>
     </div>
   </div>
 </template>
 
 
 <script setup>
+const router = useRouter();
+
+const redirectToSignUp = () => {
+  router.push('/signup')
+}
 </script>
 <style scoped></style>
