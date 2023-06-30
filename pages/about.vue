75fde7e38e94fc006080a123de4d2a3ba741ac64
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-01 01:05:23 +0300

    add trial for hasura connection

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,23 @@
 <template>
-    <h1>About page</h1>
-    <!-- <div class="flex flex-wrap">
-      <div v-for="user in users">
-        <p>{{ user.username }}</p>
-      </div>
-    </div> -->
-  </template>
+  <h1>About page</h1>
+  <div class="flex flex-col">
+    <div v-for="user in users">
+      <p>{{ user.username }}</p>
+    </div>
+  </div>
+</template>
   
 <script setup>
-  
-  // import user from '../apollo/queries/fetchUser.gql'
-  
-  // const { data } = await useAsyncQuery(user)
-  
-  // const users = data.value.user
-  
-  // try {
-  //   console.log(data.value.user[0].username)
-  // } catch (error) {
-  //   console.log(error)
-  // }
+
+import user from '../apollo/queries/fetchUser.gql'
+
+const { data } = await useAsyncQuery(user)
+
+const users = data.value.user
+
+try {
+  console.log(data.value.user[0].username)
+} catch (error) {
+  console.log(error)
+}
 </script>
